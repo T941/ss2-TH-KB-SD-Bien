53ce4e5abd84d2714447bfe1165b8d9bbcbc46c7
tranquocsang <tranquocsang19977@gmail.com>
2019-06-24 10:20:06 +0700

    baitap

---
+++ js-bai-3.js
@@ -0,0 +1,11 @@
+let soA
+let soB
+soA = prompt("nhập vào số A")
+soB = prompt("nhập vào số B")
+let A = parseInt(soA)
+let B = parseInt(soB)
+if (A%B==0)
+    alert("A là bội số của B")
+else
+    alert("A không là bội số của B")
+
